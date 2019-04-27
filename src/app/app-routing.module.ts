24a875e00be6cc210c1bf56d870cbfcaffd91ecc
pranav <pranavverma550@gmail.com>
2019-04-27 19:13:07 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { StudentComponent } from "./student/student.component";
+import { StudentdetailsComponent } from "./studentdetails/studentdetails.component";
+const routes: Routes = [
+  { path: "", redirectTo: "student", pathMatch: "full" },
+  { path: "student", component: StudentComponent },
+  { path: "studentdetails", component: StudentdetailsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
